a04e0e259e1170ac9145efe15c3f2fcea13711a3
Simone Ferrari <49599798+Scheggia99@users.noreply.github.com>
2020-11-29 12:37:17 +0100

    Baseline Activity Home

---
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/matching/MatchingFragment.java
@@ -0,0 +1,32 @@
+package com.example.lets_findus.ui.matching;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.lets_findus.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MatchingFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_matching, container, false);
+
+        return root;
+    }
+}
