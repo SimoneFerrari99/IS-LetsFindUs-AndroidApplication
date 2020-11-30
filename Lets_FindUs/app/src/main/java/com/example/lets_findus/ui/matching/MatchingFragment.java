2a5b1a15898fa6366b68192186610aa7e1724b63
GazzFRSH <68845148+GazzFRSH@users.noreply.github.com>
2020-11-30 16:05:29 +0100

    Updated MatchingFragment, added maps and started working on moving the camera to the current location and asking for permission

--- Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/matching/MatchingFragment.java
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/matching/MatchingFragment.java
@@ -1,32 +1,110 @@
 package com.example.lets_findus.ui.matching;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.lets_findus.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.concurrent.Executors;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MatchingFragment extends Fragment {
 
+    private SupportMapFragment mapFragment;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private ActivityResultLauncher<String> requestPermissionLauncher;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_matching, container, false);
+        if (fusedLocationClient == null) {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+        }
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
+            mapFragment.getMapAsync(withLocationGranted);
+        }
+
+        // R.id.map is a FrameLayout, not a Fragment
+        getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
 
         return root;
     }
+
+    private OnMapReadyCallback withLocationGranted = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(final GoogleMap googleMap) {
+            int locationPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+            if (locationPermission == PERMISSION_GRANTED) {
+                System.out.println("CIAO MORE");
+                fusedLocationClient.getLastLocation().addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            googleMap.addMarker(new MarkerOptions().position(latLng)
+                                    .title("DIOCAN"));
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                        }
+                    }
+                });
+            } //else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            //showInContextUI(...);
+            //}
+            else {
+                requestPermissionLauncher =
+                        registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                            @SuppressLint("MissingPermission")
+                            @Override
+                            public void onActivityResult(Boolean isGranted) {
+                                if (isGranted) {
+                                    fusedLocationClient.getLastLocation().addOnSuccessListener(Executors.newSingleThreadExecutor(), new OnSuccessListener<Location>() {
+                                        @Override
+                                        public void onSuccess(Location location) {
+                                            if (location != null) {
+                                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                                googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    // Explain to the user that the feature is unavailable because the
+                                    // features requires a permission that the user has denied. At the
+                                    // same time, respect the user's decision. Don't link to system
+                                    // settings in an effort to convince the user to change their
+                                    // decision.
+                                }
+                            }
+                        });
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+        }
+    };
 }
