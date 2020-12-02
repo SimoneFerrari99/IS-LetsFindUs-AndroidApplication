560a1fe324251a6b286e860c1e1154513d985aab
GazzFRSH <68845148+GazzFRSH@users.noreply.github.com>
2020-12-02 16:04:46 +0100

    Added ask for location permission, added camera animation to the current location, started working on zooming the camera on load of the map

--- Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/matching/MatchingFragment.java
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/matching/MatchingFragment.java
@@ -2,6 +2,7 @@ package com.example.lets_findus.ui.matching;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.graphics.Point;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.lets_findus.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,8 +27,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
-
-import java.util.concurrent.Executors;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
@@ -40,13 +40,45 @@ public class MatchingFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_matching, container, false);
+
         if (fusedLocationClient == null) {
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity());
         }
         if (mapFragment == null) {
             mapFragment = SupportMapFragment.newInstance();
-            mapFragment.getMapAsync(withLocationGranted);
         }
+        mapFragment.getMapAsync(withLocationGranted);
+        requestPermissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onActivityResult(Boolean isGranted) {
+                        if (isGranted) {
+                            mapFragment.getMapAsync(new OnMapReadyCallback(){
+                                @Override
+                                public void onMapReady(final GoogleMap googleMap) {
+                                    fusedLocationClient.getLastLocation().addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                                        @Override
+                                        public void onSuccess(Location location) {
+                                            if (location != null) {
+                                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                                googleMap.addMarker(new MarkerOptions().position(latLng)
+                                                        .title("CiaoSimo"));
+                                                googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+                        } else {
+                            // Explain to the user that the feature is unavailable because the
+                            // features requires a permission that the user has denied. At the
+                            // same time, respect the user's decision. Don't link to system
+                            // settings in an effort to convince the user to change their
+                            // decision.
+                        }
+                    }
+                });
 
         // R.id.map is a FrameLayout, not a Fragment
         getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
@@ -59,7 +91,7 @@ public class MatchingFragment extends Fragment {
         public void onMapReady(final GoogleMap googleMap) {
             int locationPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION);
             if (locationPermission == PERMISSION_GRANTED) {
-                fusedLocationClient.getLastLocation().addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
+                fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY, null).addOnSuccessListener(requireActivity(), new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
@@ -67,41 +99,11 @@ public class MatchingFragment extends Fragment {
                             googleMap.addMarker(new MarkerOptions().position(latLng)
                                     .title("CiaoSimo"));
                             googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                            googleMap.animateCamera(CameraUpdateFactory.zoomBy(10.0f, new Point((int)latLng.latitude, (int)latLng.longitude)));
                         }
                     }
                 });
-            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // In an educational UI, explain to the user why your app requires this
-            // permission for a specific feature to behave as expected. In this UI,
-            // include a "cancel" or "no thanks" button that allows the user to
-            // continue using your app without granting the permission.
-            //showInContextUI(...);
-            }
-            else {
-                requestPermissionLauncher =
-                        registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
-                            @SuppressLint("MissingPermission")
-                            @Override
-                            public void onActivityResult(Boolean isGranted) {
-                                if (isGranted) {
-                                    fusedLocationClient.getLastLocation().addOnSuccessListener(Executors.newSingleThreadExecutor(), new OnSuccessListener<Location>() {
-                                        @Override
-                                        public void onSuccess(Location location) {
-                                            if (location != null) {
-                                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                                                googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                                            }
-                                        }
-                                    });
-                                } else {
-                                    // Explain to the user that the feature is unavailable because the
-                                    // features requires a permission that the user has denied. At the
-                                    // same time, respect the user's decision. Don't link to system
-                                    // settings in an effort to convince the user to change their
-                                    // decision.
-                                }
-                            }
-                        });
+            } else {
                 requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
             }
         }
