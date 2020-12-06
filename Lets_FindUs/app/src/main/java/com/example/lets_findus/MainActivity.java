fe51f705539f7a710c3c0228b867c8d9198cbbe0
tulio98 <casarotti30@gmail.com>
2020-12-06 03:37:01 +0100

    Risoluzione in parte di bug sulle permission

--- Lets_FindUs/app/src/main/java/com/example/lets_findus/MainActivity.java
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/MainActivity.java
@@ -4,13 +4,17 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -21,13 +25,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.net.ConnectException;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int ENABLE_BLUETOOTH_REQUEST_CODE = 1;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 2;
-    BluetoothAdapter bluetoothAdapter;
+    BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+    BluetoothAdapter bluetoothAdapter = mBluetoothManager.getAdapter();
     Button Scan_Button = findViewById(R.id.scan_button);
-    private Boolean isLocationPermissionGranted = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+    private final Boolean isLocationPermissionGranted = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +50,18 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        Scan_Button.setOnClickListener(startBleScan());
+
+
+        Scan_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBleScan();
+            }
+
+        });
+
     }
 
-    private void promptEnableBluetooth() {
-        if(!bluetoothAdapter.isEnabled()) {
-            Intent enableByIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableByIntent, ENABLE_BLUETOOTH_REQUEST_CODE);
-        }
-    }
 
     @Override
     public void onResume(){
@@ -64,18 +74,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case ENABLE_BLUETOOTH_REQUEST_CODE :
-                if(resultCode != Activity.RESULT_OK) {
-                    promptEnableBluetooth();
+        if (requestCode == ENABLE_BLUETOOTH_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK) {
+                promptEnableBluetooth();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startBleScan();
+                }
+                else {
+                    requestLocationPermission();
                 }
         }
     }
 
-    private Boolean hasPermission(String accessFineLocation) {
-        return ContextCompat.checkSelfPermission(this, accessFineLocation) == PackageManager.PERMISSION_GRANTED;
-    }
 
+
+    private void promptEnableBluetooth() {
+        if(!bluetoothAdapter.isEnabled()) {
+            Intent enableByIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableByIntent, ENABLE_BLUETOOTH_REQUEST_CODE);
+        }
+    }
 
     private void startBleScan() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !isLocationPermissionGranted) {
@@ -85,26 +112,37 @@ public class MainActivity extends AppCompatActivity {
             /* TODO: Actually perform scan */
         }
     }
+
     private void requestLocationPermission() {
         if(isLocationPermissionGranted)
             return;
-        runOnUiThread(new Thread(new Runnable() {
+        runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                new AlertDialog(getApplicationContext())
+                new AlertDialog.Builder(getApplicationContext())
                         .setTitle("Location permission required")
                         .setMessage("Starting from Android M (6.0), the system requires apps to be granted " +
                                 "location access in order to scan for BLE devices.")
                         .setCancelable(false)
-                        .setPositiveButton(android.R.string.ok) {
-                                requestPermissions(
-                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        LOCATION_PERMISSION_REQUEST_CODE
-                                );
-                }
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+                            }
+                        }).show();
+                /* TODO: Implements .setNegativeButton */
             }
-        }));
+        });
     }
+
+
+    private Boolean hasPermission(String accessFineLocation) {
+        return ContextCompat.checkSelfPermission(getApplicationContext(), accessFineLocation) == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+
+
 }
 
 
