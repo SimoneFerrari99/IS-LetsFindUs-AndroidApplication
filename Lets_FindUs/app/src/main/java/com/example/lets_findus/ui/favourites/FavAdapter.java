d50e0d611b74df0ad63c84d1b3cc182e79bd83cb
GazzFRSH <68845148+GazzFRSH@users.noreply.github.com>
2020-12-09 10:24:04 +0100

    Added list to favourite section using recycler view and card view

---
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/favourites/FavAdapter.java
@@ -0,0 +1,63 @@
+package com.example.lets_findus.ui.favourites;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lets_findus.R;
+import com.example.lets_findus.utilities.Meeting;
+import com.example.lets_findus.utilities.Person;
+
+import java.util.ArrayList;
+
+public class FavAdapter extends RecyclerView.Adapter<FavAdapter.MyViewHolder>{
+
+    private ArrayList<Meeting<Person>> dataSet;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewName;
+        TextView textViewVersion;
+        ImageView imageViewIcon;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            this.textViewName = itemView.findViewById(R.id.textViewName);
+            this.textViewVersion = itemView.findViewById(R.id.textViewVersion);
+            this.imageViewIcon = itemView.findViewById(R.id.imageView);
+        }
+    }
+
+    public FavAdapter(ArrayList<Meeting<Person>> data) {
+        this.dataSet = data;
+    }
+
+    @NonNull
+    @Override
+    public FavAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cards_layout, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavAdapter.MyViewHolder holder, int position) {
+        TextView textViewName = holder.textViewName;
+        TextView textViewVersion = holder.textViewVersion;
+        ImageView imageView = holder.imageViewIcon;
+        textViewName.setText("Nickname: "+dataSet.get(position).data.nickname);
+        textViewVersion.setText("Anno di nascita: "+String.valueOf(dataSet.get(position).data.yearOfBirth));
+        //imageView.setImageResource(dataSet.get(position).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+}
--- Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/favourites/FavouritesFragment.java
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/favourites/FavouritesFragment.java
@@ -4,23 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.lets_findus.R;
+import com.example.lets_findus.utilities.Meeting;
+import com.example.lets_findus.utilities.Person;
+
+import java.util.ArrayList;
 
 public class FavouritesFragment extends Fragment {
 
+    private static RecyclerView.Adapter adapter; //l'adapter serve per popolare ogni riga
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<Meeting<Person>> data;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_favourites, container, false);
 
+        recyclerView = root.findViewById(R.id.rec_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(root.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<>();
+        data.add(new Meeting<Person>(new Person("", "Gazz", Person.Sex.MALE, 1999), null));
+        data.add(new Meeting<Person>(new Person("", "Scheggia", Person.Sex.MALE, 1999), null));
+        data.add(new Meeting<Person>(new Person("", "Tulio", Person.Sex.MALE, 1999), null));
+        data.add(new Meeting<Person>(new Person("", "Ciullia", Person.Sex.FEMALE, 1999), null));
+        for (int i = 0; i < 10; i++){
+            data.add(new Meeting<Person>(new Person("", "Toso", Person.Sex.MALE, 1999), null));
+        }
+
+        adapter = new FavAdapter(data);
+        recyclerView.setAdapter(adapter);
         return root;
     }
 }
