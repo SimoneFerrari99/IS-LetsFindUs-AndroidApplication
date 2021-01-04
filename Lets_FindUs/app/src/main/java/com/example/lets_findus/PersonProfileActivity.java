8893526a2d55f6ad4e165ca5a28535472d44d964
Giulio Trolese <giuliotrolese99@gmail.com>
2021-01-04 09:51:42 +0100

    Started adding activity for viewing profile

---
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/PersonProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.lets_findus;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PersonProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_profile);
+    }
+}
