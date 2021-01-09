ca2949040a20d7a2803b01eaca5347a4f6755756
ciullia <71281373+ciullia@users.noreply.github.com>
2021-01-09 19:47:39 +0100

    Update Filter

--- Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/filter/Filter.java
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/ui/filter/Filter.java
@@ -13,20 +13,43 @@ import com.example.lets_findus.R;
 
 public class Filter extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String[] country = {"India", "USA", "China", "Japan", "Other"};
+    /*String[] date = {"Oggi", "Ieri"};
+    String[] hour = {"12:30", "16:30"};
+    String[] sex = {"Maschio", "Femmina", "Altro"};
+    String[] age = {"18", "30", "50", "70"};*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.filter_popup_window);   penso sia sbagliato la parte sotto
         setContentView(R.layout.activity_main);
         //Getting the instance of Spinner and applying OnItemSelectedListener on it
         Spinner spin = (Spinner) findViewById(R.id.spinner_date);
+        Spinner spin_date = (Spinner) findViewById(R.id.spinner_date);
+        Spinner spin_hour = (Spinner) findViewById(R.id.spinner_hour);
+        Spinner spin_sex = (Spinner) findViewById(R.id.spinner_sex);
+        Spinner spin_age = (Spinner) findViewById(R.id.spinner_age);
+
         spin.setOnItemSelectedListener(this);
 
         //Creating the ArrayAdapter instance having the country list
         ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, country);
+        /*ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, date);
+        ArrayAdapter bb = new ArrayAdapter(this, android.R.layout.simple_spinner_item, hour);
+        ArrayAdapter cc = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sex);
+        ArrayAdapter dd = new ArrayAdapter(this, android.R.layout.simple_spinner_item, age);*/
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        /*aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cc.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dd.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);*/
+
         //Setting the ArrayAdapter data on the Spinner
         spin.setAdapter(aa);
+        /*spin_date.setAdapter((aa));
+        spin_hour.setAdapter((bb));
+        spin_sex.setAdapter(cc);
+        spin_age.setAdapter(dd);*/
 
     }
 
