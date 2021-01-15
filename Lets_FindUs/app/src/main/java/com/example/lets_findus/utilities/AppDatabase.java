f9f538db69f003e8a15808e914e6114335afff2c
Giulio Trolese <68845148+GiulioTr@users.noreply.github.com>
2021-01-15 21:16:12 +0100

    Updated room version

--- Lets_FindUs/app/src/main/java/com/example/lets_findus/utilities/AppDatabase.java
+++ Lets_FindUs/app/src/main/java/com/example/lets_findus/utilities/AppDatabase.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 //classe che specifica il database, le sue istanze e i relativi dao
-@Database(entities = {Person.class, Meeting.class}, version = 1)
+@Database(entities = {Person.class, Meeting.class}, version = 1.01)
 @TypeConverters({DateConverter.class, SexConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract PersonDao personDao();
